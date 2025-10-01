e2ea3454c12b3a74ee28a0fdc92445e33f5223dd
rupjyoti921 <rupjyoti921@gmail.com>
2025-10-01 14:30:00 +0530

    added all required models

---
+++ CSharpMiniShop/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMiniShop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Title {  get; set; }
+        public double Price { get; set; }
+        public string? Description { get; set; }
+        public string? Category {  get; set; }
+        public List<Rating>? Ratings { get; set; }
+    }
+}
